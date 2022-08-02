7cdde7649b81d68846b03ec84e8e921fd5164b2e
Yusuf Ali Karaca <yusufkaraca1571@protonmail.com>
2022-08-02 19:25:02 +0300

    initial

--- src/pages/CityList.js
+++ src/pages/CityList.js
@@ -2,28 +2,9 @@ import cityList from "../utils/cityList";
 import SearchSection from "../components/SearchSection";
 import { useState } from "react";
 function CityList() {
-  const [input, setInput] = useState("qwe");
+  const [input, setInput] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const handleChange = (event) => {
-    const filter = (city) => {
-      setInput(event.target.value);
-      if (input != "") {
-        return Object.values(city).join(" ").toLowerCase().includes(input);
-      }
-    };
-    const newCityList = cityList.filter(filter);
-
-    const list = newCityList.map((city) => {
-      return (
-        <div className="inline-flex p-2 gap-4 bg-slate-500 ">
-          <div>{city.plaka}</div>
-          <div>{city.il}</div>
-        </div>
-      );
-    });
-    setSearchResults(list);
-    console.log(newCityList);
-  };
+  const handleSearch = () => {};
   const list = cityList.map((city) => {
     return (
       <div className="inline-flex p-2 gap-4 bg-slate-500 ">
@@ -32,17 +13,17 @@ function CityList() {
       </div>
     );
   });
+
   return (
     <div className="grid items-center text-center justify-center place-content-center">
       <input
         value={input}
-        onChange={handleChange}
         type="text"
-        className="py-3 px-2 bg-slate-200 mb-3"
+        className="py-3 px-2 outline-none focus:outline-slate-700 bg-slate-200 mb-3"
       />
+      <p>{input}</p>
       <div className="grid grid-cols-2 gap-4 max-w-4xl md:grid-cols-3 lg:grid-cols-4">
         {searchResults}
-        {input.lenght < 1}
       </div>
       <li>Deneme2</li>
     </div>
